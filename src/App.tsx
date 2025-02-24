6a895bfa2883aa27d291172ed89cdcff656403bd
Hydradevx <themaskedprogrammer1234@gmail.com>
2025-02-24 17:17:12 +0530

    Initailized Project with Bun

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+export function App() {
+  return (
+    <div> 
+      <h1 className="text-3xl font-bold underline" >
+        Hi Hydra Here!!!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
